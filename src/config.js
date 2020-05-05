1f6095053694a43a9a4618c8b6866f27098347ea
Bruno Rodrigues <Bruno.rsilva002@gmail.com>
2020-05-05 01:20:43 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "bucket-up-app"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://r0oiw85rf5.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_2rieaX44i",
+    APP_CLIENT_ID: "5nbru6f8kjb9fqjqh1pf5jhfhb",
+    IDENTITY_POOL_ID: "us-east-1:d42279b1-404c-4942-83cf-d442c26cecf7"
+  }
+};
